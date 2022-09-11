36e65fe2a8d6dc5a86ec80fc50830e0649a28929
Devan Kavalchek <devankav7@gmail.com>
2022-09-11 18:48:21 -0400

    Stubbed project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Tile from './components/tile';
+
+function App() {
+  return (
+    <div className='App'>
+      <Tile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/tile.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tile = (props) => {
+  return <div className='tile'></div>;
+};
+
+export default Tile;
