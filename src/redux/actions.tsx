b29f757d4bee673cdc3c180da85e3c6ec396dec6
Devan Kavalchek <devankav7@gmail.com>
2022-10-14 19:11:13 -0400

    Stubbed win reducer

--- src/redux/actions.tsx
+++ src/redux/actions.tsx
@@ -1,4 +1,6 @@
 export const BOARD_CREATE_EMPTY = 'board/createEmpty';
 export const BOARD_GENERATE = 'board/generateBoard';
+export const BOARD_OPEN_TILE = 'board/openTile';
+export const BOARD_OPEN_RECURSIVE = 'board/openTileRecursive';
 
 export const GAME_STATE_SET_LOSS = 'gameState/setGameLoss';
--- src/redux/slices/game_state_slice.tsx
+++ src/redux/slices/game_state_slice.tsx
@@ -1,5 +1,11 @@
-import {createSlice} from '@reduxjs/toolkit';
-import {BOARD_CREATE_EMPTY, BOARD_GENERATE} from 'redux/actions';
+import {createSlice, current} from '@reduxjs/toolkit';
+import {
+  BOARD_CREATE_EMPTY,
+  BOARD_GENERATE,
+  BOARD_OPEN_RECURSIVE,
+  BOARD_OPEN_TILE,
+} from 'redux/actions';
+import store from 'redux/store';
 import {
   GameState,
   GAME_STATE_IN_PROGRESS,
@@ -8,6 +14,10 @@ import {
 } from '../../types';
 
 const initialState: GameState = 'NEW_GAME';
+
+const winReducer = (state) => {
+  return state;
+};
 
 const gameStateSlice = createSlice({
   name: 'gameState',
@@ -28,6 +38,8 @@ const gameStateSlice = createSlice({
     [BOARD_GENERATE]: (state) => GAME_STATE_IN_PROGRESS,
 
     // TODO: open tile: if all tiles are satisfied and numFlags = numMines, then win
+    [BOARD_OPEN_RECURSIVE]: winReducer,
+    [BOARD_OPEN_TILE]: winReducer,
   },
 });
 
