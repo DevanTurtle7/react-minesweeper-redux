0d952033d88783a53112d9772088b57b01475fa1
Devan Kavalchek <devankav7@gmail.com>
2022-10-15 00:49:58 -0400

    Added comment

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,6 +2,8 @@ import {configureStore} from '@reduxjs/toolkit';
 import board_reducers from 'redux/slices/board_slice';
 import game_state_reducers from 'redux/slices/game_state_slice';
 
+// TODO: Add preferences slice
+
 export default configureStore({
   reducer: {
     board: board_reducers,
