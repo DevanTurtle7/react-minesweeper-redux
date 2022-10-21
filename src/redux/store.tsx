8f3cafc5a4397fbdc745da81a0bd1aee88f82e01
Devan Kavalchek <devankav7@gmail.com>
2022-10-20 23:32:56 -0400

    Removed comment

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -3,7 +3,6 @@ import board_reducers from 'redux/slices/board_slice';
 import game_state_reducers from 'redux/slices/game_state_slice';
 import preferences_reducers from './slices/preferences_slice';
 
-// TODO: Add preferences slice
 // TODO: Currently selected slice. Add neighbors on click down to highlight them
 
 export default configureStore({
